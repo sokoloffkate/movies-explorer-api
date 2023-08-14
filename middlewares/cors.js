e3da702c04b3dba5cd51313e72bcdf63bec58b32
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-08-15 05:09:25 +1000

    Change port

---
+++ middlewares/cors.js
@@ -0,0 +1,26 @@
+const allowedCors = [
+  'https://movie.sokolova.nomoredomains.monster',
+  'http://movie.sokolova.nomoredomains.monster',
+  'localhost:3005',
+];
+
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  console.log(req.header.origin);
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+};
--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 module.exports.rateLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 100,
+  max: 1000,
   message: 'Превышено ограничение количества запросов за сутки',
   standardHeaders: true,
   legacyHeaders: false,
