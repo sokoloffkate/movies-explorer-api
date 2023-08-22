cdb7213b69c6704b29cc05bbaaceb8433d244b69
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-08-23 05:26:19 +1000

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const Unauthorised = require('../errors/Unauthorised');
+const Conflict = require('../errors/Conflict');
 const { JWT_SECRET_DEV } = require('../utils/constants');
 
 const { JWT_SECRET, NODE_ENV } = process.env;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
+
   if (!token) {
-    return next(new Unauthorised('Необходима авторизация'));
+    return next(new Conflict('Необходима авторизация111'));
   }
 
   let payload;
